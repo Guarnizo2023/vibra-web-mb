ca727f12c1ec509b489bbc36af10d1657e08f2d4
Guarnizo2023 <mottaermes@gmail.com>
2025-02-21 12:44:49 -0500

    feat: Adjustment in form register

--- app/components/users/RegisterForm.tsx
+++ app/components/users/RegisterForm.tsx
@@ -1,6 +1,7 @@
 import { Picker } from '@react-native-picker/picker';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import { useRouter } from 'expo-router';
+import React, { useEffect, useId, useState } from 'react';
 import { Alert, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { useTailwind } from 'tailwind-rn';
 import api from '../../services/api';
@@ -13,13 +14,33 @@ const RegisterForm = () => {
     const [typeDocument, setTypeDocument] = useState('');
     const [email, setEmail] = useState('');
     const [role, setRole] = useState('');
-    const [hightSchool, setHightSchool] = useState('');
+    const [hightSchool, setHightSchool] = useState('0');
     const [course, setCourse] = useState('');
     const [typeDocumentOptions, setTypeDocumentOptions] = useState([]);
     const [roleOptions, setRoleOptions] = useState<any>([]);
     const [hightSchoolOptions, setHightSchoolOptions] = useState<any>([]);
     const [courseOptions, setCourseOptions] = useState<any>([]);
     const [loading, setLoading] = useState(false);
+    const [isValidateForm, setIsValidateForm] = useState<boolean>(false);
+    const router = useRouter();
+
+    useEffect(() => {
+        if(username && password && documentNumber && typeDocument && email && role && course){
+            setIsValidateForm(true);
+        }
+
+    }, [username,password,documentNumber,typeDocument,email,role, course]);
+
+    useEffect(() => {
+        const fetchOptions_ = async () => {
+        if(hightSchool){
+            const hightSchoolResponse: any = await axios.get(`http://localhost:4000/courses/allByHightSchool/${hightSchool}`);
+            setCourseOptions([{ id: '0', name: 'Seleccione un curso' }, ...hightSchoolResponse.data ]);
+        }
+    }
+    fetchOptions_();
+
+    }, [hightSchool]);
 
     useEffect(() => {
         const fetchOptions = async () => {
@@ -37,9 +58,9 @@ const RegisterForm = () => {
                 const cursosResponse: any = await axios.get('http://localhost:4000/courses/allByHightSchool');
                 console.log('roleResponse?.data:', roleResponse?.data);
                 setTypeDocumentOptions(typeDocumentResponse?.data);
-                setRoleOptions([...roleResponse.data, { id: '0', name: 'Seleccione un tipo' }]);
-                setHightSchoolOptions([...hightSchoolResponse.data, { id: '0', name: 'Seleccione una institución' }]);
-                setCourseOptions([...roleResponse.data, { id: '0', name: 'Seleccione un curso' }]);
+                setRoleOptions([{ id: '0', name: 'Seleccione un tipo' }, ...roleResponse.data ]);
+                setHightSchoolOptions([{ id: '0', name: 'Seleccione una institución' }, ...hightSchoolResponse.data]);
+                setCourseOptions([{ id: '0', name: 'Seleccione un curso' }, ...cursosResponse.data ]);
             } catch (error) {
                 console.error('Error fetching options', error);
             }
@@ -48,6 +69,10 @@ const RegisterForm = () => {
         fetchOptions();
     }, []);
 
+    const handleCancel = async () => {
+        router.push('../login/loginForm');
+    }
+
     const handleRegister = async () => {
         if (!username || !password || !documentNumber || !typeDocument || !email || !role || !course) {
             Alert.alert('Error', 'Por favor, completa todos los campos.');
@@ -55,11 +80,14 @@ const RegisterForm = () => {
         }
 
         setLoading(true);
-
+        const uniqueID = 5; 
         try {
-            const response = await api.post('/users', { username, password, documentNumber, typeDocument, email, role, course});
-            Alert.alert('Éxito', 'Registro de usuario exitoso.');
-            console.log('Respuesta de la API:', response.data);
+            // console.log('data:',{ username, password, documentNumber, typeDocument, email, role, course, hightSchool});
+           const response = await api.post('/users/create', { id: uniqueID, username, password, documentNumber, typeDocument, email, role, course, hightSchool, avatar:'04.jpg'});
+            if(response) {
+                Alert.alert('Éxito', 'Registro de usuario exitoso.');
+                console.log('Respuesta de la API:', response.data);
+            }
         } catch (error) {
             Alert.alert('Error', 'Campos incompletos');
             console.error('Error en la solicitud:', error);
@@ -136,7 +164,7 @@ const RegisterForm = () => {
                 onValueChange={(itemValue) => setRole(itemValue)}
             >
                 {roleOptions?.map((option: any) => (
-                    <Picker.Item key={option.id} label={option.name} value={option.id} />
+                    <Picker.Item key={option._id} label={option.name} value={option._id} />
                 ))}
             </Picker>
             <Text style={tailwind('text-10 font-bold text-left mb-0 text-white mt-2')}>
@@ -145,34 +173,46 @@ const RegisterForm = () => {
             <Picker
                 selectedValue={hightSchool}
                 style={[styles.input, tailwind('w-full px-3 py-2 border border-gray-300 rounded-md mb-4 my-1 bg-white')]}
-                onValueChange={(itemValue) => setTypeDocument(itemValue)}
+                onValueChange={(itemValue: any) => {
+                    console.log('itemValue: ', itemValue);
+                    setHightSchool(itemValue);
+                }}
             >
                 {hightSchoolOptions.map((option: any) => (
-                    <Picker.Item key={option.id} label={option.name} value={option.name} />
+                    <Picker.Item key={option._id} label={option.name} value={option._id} />
                 ))}
             </Picker>
-            {typeDocument == "3" && <>
+            {hightSchool !== '0' && <>
                 <Text style={tailwind('text-10 font-bold text-left mb-0 text-white mt-2')}>
                     Curso
                 </Text>
                 <Picker
                     selectedValue={course}
                     style={[styles.input, tailwind('w-full px-3 py-2 border border-gray-300 rounded-md mb-4 my-2 bg-white')]}
-                    onValueChange={(itemValue) => setTypeDocument(itemValue)}
+                    onValueChange={(itemValue) => setCourse(itemValue)}
                 >
                     {courseOptions.map((option: any) => (
-                        <Picker.Item key={option.id} label={option.name} value={option.name} />
+                        <Picker.Item key={option._id} label={option.name} value={option._id} />
                     ))}
                 </Picker>
             </>}
-            <View style={tailwind('mt-4')}>
+            <View style={tailwind('flex-row justify-center items-center mt-4 w-full')}>
                 <TouchableOpacity
-                    style={tailwind('flex-1 w-full bg-red-500 p-3 rounded-md items-center')}
-                    onPress={handleRegister}
+                    style={tailwind('flex-1 w-full bg-gray-500 p-3 rounded-md items-center mr-3')}
+                    onPress={handleCancel}
                     disabled={false}
                 >
                     <Text style={tailwind('text-white font-bold text-center')}>
-                        {'Generar nuevo usuario'}
+                        {'Cancelar'}
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={tailwind(`flex-1 w-full ${isValidateForm ? 'bg-red-500 text-white':'bg-gray-300 text-black'} p-3 rounded-md items-center`)}
+                    onPress={handleRegister}
+                    disabled={false}
+                >
+                    <Text style={tailwind(`font-bold text-center ${isValidateForm ? 'text-white':'text-black'}`)}>
+                        {'Generar usuario'}
                     </Text>
                 </TouchableOpacity>
             </View>
